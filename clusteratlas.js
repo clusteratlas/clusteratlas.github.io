231b734965d3418a3c896b4d3f3ad0e6c1dcf07d
clusteratlas <clusteratlas@hotmail.com>
2017-06-18 20:48:53 +0800

    yay!

--- clusteratlas.js
+++ clusteratlas.js
@@ -1,30 +1,20 @@
-Ladderized
-	.css({
-		link: 'https://unpkg.com/materialize-css@0.98.2/dist/css/materialize.min.css'
-	})
-	.load(function(){
-		Ladderized
-			.clearQueue()
-			.showNanoBar()
-			.js({
-				link: 'https://unpkg.com/jquery@3.2.1/dist/jquery.min.js'	
-			})
-			.js({
-				link: 'https://unpkg.com/materialize-css@0.98.2/dist/js/materialize.min.js'
-			})
-			.load(function(){
-				console.log('All files loaded!');
-			});
-	});
-	/*
-	.clearQueue()
-	.showNanoBar()
-	.js({
-		link: 'https://unpkg.com/jquery@3.2.1/dist/jquery.min.js'	
-	})
-	.js({
-		link: 'https://unpkg.com/materialize-css@0.98.2/dist/js/materialize.min.js'
-	})
-	.load(function(){
-		console.log('All files loaded!');
-	})*/
+
+	Ladderized
+		.css({
+			link: 'https://unpkg.com/materialize-css@0.98.2/dist/css/materialize.min.css'
+		})
+		.load(function(){
+			Ladderized
+				.clearQueue()
+				.createNanoBar(2)
+				.js({
+					link: 'https://unpkg.com/jquery@3.2.1/dist/jquery.min.js'
+				})
+				.js({
+					link: 'https://unpkg.com/materialize-css@0.98.2/dist/js/materialize.min.js'
+				})
+				.load(function(){
+					console.log('All files loaded!');
+					console.log(window);
+				});
+		});
