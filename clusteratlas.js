9dbfa0b322a53d8a7feafda3d81c6302adfbeacf
clusteratlas <clusteratlas@hotmail.com>
2017-06-18 18:51:57 +0800

    update

--- clusteratlas.js
+++ clusteratlas.js
@@ -1,16 +1,16 @@
 Ladderized
 	.css({
-		link: 'https://unpkg.com/materialize-css/dist/css/materialize.min.css'
+		link: 'https://unpkg.com/materialize-css@0.98.2/dist/css/materialize.min.css'
 	})
 	.load(function(){
 		Ladderized
 			.clearQueue()
 			.showNanoBar()
 			.js({
-				link: 'https://unpkg.com/jquery/dist/jquery.min.js'	
+				link: 'https://unpkg.com/jquery@3.2.1/dist/jquery.min.js'	
 			})
 			.js({
-				link: 'https://unpkg.com/materialize-css/dist/js/materialize.min.js'
+				link: 'https://unpkg.com/materialize-css@0.98.2/dist/js/materialize.min.js'
 			})
 			.load(function(){
 				console.log('All files loaded!');
